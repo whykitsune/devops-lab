7d5dea5829ac18af11dbf9af8d0258a41ab783f4
whykitsune <mistermax05@mail.ru>
2025-04-06 14:42:34 +0300

    fix relative import

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 
-from .settings import settings
-from .routers import router
+from src.settings import settings
+from src.routers import router
 
 app = FastAPI(debug=False)
 app.include_router(router=router)
