0ecb4f62cc56093359dbef2332d515dbb2504cad
whykitsune <mistermax05@mail.ru>
2025-04-06 13:54:45 +0300

    modified tests and added workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,45 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        'name': 'Boris Bidjan',
+        'email': 'b.b.borisov@mail.com'
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
+
+    # get_response = client.get("/api/v1/user", params={'email': new_user['email']})
+    # assert get_response.status_code == 200
+    # assert get_response.json()['email'] == new_user['email']
+    # assert get_response.json()['name'] == new_user['name']
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    existing_user = {
+        'name': 'Petya',
+        'email': users[0]['email']
+    }
+    response = client.post("/api/v1/user", json=existing_user)
+    assert response.status_code == 409
+    assert response.json() == {'detail': 'User with this email already exists'}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    temp_user = {
+        'name': 'Kolya Kolkin',
+        'email': 'k.k.kolkin@mail.com'
+    }
+    create_response = client.post("/api/v1/user", json=temp_user)
+    
+    delete_response = client.delete("/api/v1/user", params={'email': temp_user['email']})
+    assert delete_response.status_code == 204
+    
+    get_response = client.get("/api/v1/user", params={'email': temp_user['email']})
+    assert get_response.status_code == 404
